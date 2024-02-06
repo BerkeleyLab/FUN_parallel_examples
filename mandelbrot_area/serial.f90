6205c8455c7bf25d08d6c7740e1f95d118b7decf
Brad Richardson <everythingfunctional@protonmail.com>
2024-02-06 14:25:22 -0600

    create serial version of mandelbrot area

---
+++ mandelbrot_area/serial.f90
@@ -0,0 +1,45 @@
+! Credit to: https://fortran-lang.discourse.group/t/fortran-code-snippets/2150/72
+program mandelbrot_area
+    ! compile and run with:
+    !   $ ftn serial.f90 -o serial.exe
+    !   $ ./serial.exe
+    implicit none
+
+    integer, parameter :: rk = selected_real_kind(15, 300)
+    integer, parameter :: n = 2**12
+    real(rk), parameter :: expected = 1.5065849_rk
+    integer :: i, j
+    real(rk) :: area, re, im
+
+    area = 0
+
+    do i = 1, n
+        re = -2._rk + 2.49_rk * real(i-1,rk) / real(n,rk)
+        do j = 1, n/2
+            im = 1.15_rk * real(j-1,rk) / real(n/2,rk)
+            if (re**2 + im**2 > 4._rk) cycle
+            if (julia(cmplx(re,im,rk))) area = area + 1._rk
+        end do
+    end do
+
+    area = 4._rk * 2.49_rk * 1.15_rk * area / real(n,rk)**2
+    print *, "Area: ", area, ", expected: ", expected, ", difference: ", expected-area
+contains
+    pure function julia(c)
+        complex(rk), intent(in) :: c
+        logical :: julia
+
+        integer :: i
+        complex(rk) :: z
+
+        z=c
+        do i = 1, n/8
+            z= (((((((z**2 + c)**2 + c)**2 + c)**2 + c)**2 + c)**2 + c)**2 + c)**2 + c
+            if (z%re**2 + z%im**2 > 4._rk) then
+                julia = .false.
+                return
+            end if
+        end do
+        julia=.true.
+    end function
+end program
